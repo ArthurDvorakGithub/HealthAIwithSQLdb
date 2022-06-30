baeb106f288042373f71578c18c994848ae7a8ae
Arthur Dvorak <artdvorak@yandex.ru>
2022-06-30 14:25:28 +0300

    create product and category

---
+++ HealthAI/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using HealthAI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthAI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+
+        public DbSet<Product> Product { get; set; }
+    }
+}
